164801ee38e2c16e25378d927702dba99be9f790
Nexus Prime <nicolas.estevao.lima1004@hotmail.com>
2022-09-10 05:16:57 -0300

    global structures

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-export { default as Command } from './core/structures/Command';
-export { default as Event } from './core/structures/Event';
+import Command from './core/structures/Command';
+import Event from './core/structures/Event';
+
+declare global {
+    var WuCommand: typeof Command;
+    var WuEvent: typeof Event;
+}
+
+global.WuCommand = Command;
+global.WuEvent = Event;
+
+import './core'
